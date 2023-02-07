bf1a9ee57aa36e6498e2e0107d9a5a728ef27be4
lhsll <82965551+lhsll@users.noreply.github.com>
2023-02-07 20:35:45 +0800

    更新 WeiBocdn.js

--- WeiBocdn.js
+++ WeiBocdn.js
@@ -1,11 +1,11 @@
-let ipv1 = "125.64.128.248"
-let ipv2 = "125.64.128.240"
-let ipv3 = "125.64.128.241"
-let ipv4 = "125.64.128.242"
-let ipv5 = "125.64.128.243"
-let ipv6 = "125.64.128.244"
-let ipv7 = "125.64.128.239"
-let ipv8 = "125.64.128.238"
+let ipv1 = "220.181.135.228"
+let ipv2 = "220.181.135.229"
+let ipv3 = "220.181.135.225"
+let ipv4 = "220.181.135.227"
+let ipv5 = "220.181.135.226"
+let ipv6 = "220.181.135.224"
+let ipv7 = "220.181.135.231"
+let ipv8 = "220.181.135.230"
 
 if ($domain = 'tvax1.sinaimg.cn','tvax2.sinaimg.cn','tvax3.sinaimg.cn') {
     $done({addresses: [ipv1,ipv2,ipv3,ipv4,ipv5,ipv6,ipv7,ipv8]})
